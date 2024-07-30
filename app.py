edecfd405db899c4f2fcad420cc8bd2689f15063
matemarodi <123512490+matemarodi@users.noreply.github.com>
2024-07-30 10:14:35 +0000

    added dynamic data

--- app.py
+++ app.py
@@ -1,11 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+EVENTS = [
+    {
+        'id': 1,
+        'title': 'selection middle',
+        'location': 'Zuppa',
+        'date': '2025-07-24'
+    },
+    {
+        'id': 2,
+        'title': 'selection long',
+        'location': 'Zuppa',
+        'date': '2025-07-25'
+    },
+    {
+        'id': 3,
+        'title': 'ROB',
+        'location': 'Szentes',
+        #'date': '2025-09-20'
+    }
+]
 
 
 @app.route("/")
 def hello_world():
-  return "<p>Hello, World!</p>"
+    #return "<p>Hello, World!</p>"
+    return render_template('home.html', events=EVENTS)
+
+
+@app.route("/api/events")
+def list_events():
+    return jsonify(EVENTS)
+
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
